269ede498ade031b8a04f8432f30416409945cdd
DaanChoi <104367835+DaanChoi@users.noreply.github.com>
2022-07-07 08:40:58 +0900

    Update kiosk.c

--- kiosk.c
+++ kiosk.c
@@ -10,7 +10,7 @@ product;
 
 int main(void) {
 	int menu, idx = 0;
-	product pro[10] = { 0 }; // 품목은 최대 10개의 종류까지 선택 가능
+	product prd[10] = { 0 }; // 품목은 최대 10개의 종류까지 선택 가능
 
 	printf("JUICY KIOSK");
 	printf("\n====== menu ======\n");
@@ -21,58 +21,68 @@ int main(void) {
 	
 
 	if (menu == 1) {
-		printf("\nSEOSON\n");
+		printf("\n[SEOSON]\n");
+		printf("\nM : 3,500원\n");
+		printf("\n수박살사믹스\n수박살사큐브\n생딸라떼\n봄청블라썸\n\n");
 
-		printf("품목을 선택하세요 : ");
-		scanf("%s", &pro[idx].name);
+		printf("품목명을 입력하세요 : ");
+		scanf("%s", &prd[idx].name);
 		printf("수량을 입력하세요 : ");
-		scanf("%d", &pro[idx].qty);
+		scanf("%d", &prd[idx].qty);
 		printf("사이즈를 입력하세요(M) : ");
-		scanf("%s", &pro[idx].size);
+		scanf("%s", &prd[idx].size);
 		idx++;
 	}
 	else if (menu == 2) {
-		printf("\nJUICE\n");
+		printf("\n[JUICE]\n");
+		printf("\nM : 1,500원 / XL : 2,800원\n");
+		printf("\n바나나\n키위\n파인애플\n초코바나나\n사과\n\n");
 
-		printf("품목을 선택하세요 : ");
-		scanf("%s", &pro[idx].name);
+		printf("품목명을 입력하세요 : ");
+		scanf("%s", &prd[idx].name);
 		printf("수량을 입력하세요 : ");
-		scanf("%d", &pro[idx].qty);
+		scanf("%d", &prd[idx].qty);
 		printf("사이즈를 입력하세요(M / XL) : ");
-		scanf("%s", &pro[idx].size);
+		scanf("%s", &prd[idx].size);
 		idx++;
 	}
 	else if (menu == 3) {
-		printf("\nJUICY COFFEE\n");
+		printf("\n[JUICY COFFEE]\n");
+		printf("\nM : 2,000원 / XL : 3,500원\n");
+		printf("\n바닐라라떼\n카페모카\n카라멜마끼야또\n연유커피\n\n");
 
-		printf("품목을 선택하세요 : ");
-		scanf("%s", &pro[idx].name);
+		printf("품목명을 입력하세요 : ");
+		scanf("%s", &prd[idx].name);
 		printf("수량을 입력하세요 : ");
-		scanf("%d", &pro[idx].qty);
+		scanf("%d", &prd[idx].qty);
 		printf("사이즈를 입력하세요(M / XL) : ");
-		scanf("%s", &pro[idx].size);
+		scanf("%s", &prd[idx].size);
 		idx++;
 	}
 	else if (menu == 4) {
-		printf("\nYOGULT\n");
+		printf("\n[YOGULT]\n");
+		printf("\nM : 2,000원 / XL : 3,800원\n");
+		printf("\n바나나요거트\n딸기요거트\n블루베리요거트\n\n");
 
-		printf("품목을 선택하세요 : ");
-		scanf("%s", &pro[idx].name);
+		printf("품목명을 입력하세요 : ");
+		scanf("%s", &prd[idx].name);
 		printf("수량을 입력하세요 : ");
-		scanf("%d", &pro[idx].qty);
+		scanf("%d", &prd[idx].qty);
 		printf("사이즈를 입력하세요(M / XL) : ");
-		scanf("%s", &pro[idx].size);
+		scanf("%s", &prd[idx].size);
 		idx++;
 	}
 	else if (menu == 5) {
-		printf("\nADE\n");
+		printf("\n[ADE]\n");
+		printf("\nM : 3,800원\n");
+		printf("\n레몬\n오렌지\n자몽\n청포도\n\n");
 
-		printf("품목을 선택하세요 : ");
-		scanf("%s", &pro[idx].name);
+		printf("품목명을 입력하세요 : ");
+		scanf("%s", &prd[idx].name);
 		printf("수량을 입력하세요 : ");
-		scanf("%d", &pro[idx].qty);
+		scanf("%d", &prd[idx].qty);
 		printf("사이즈를 입력하세요(M) : ");
-		scanf("%s", &pro[idx].size);
+		scanf("%s", &prd[idx].size);
 		idx++;
 	}
 	else {
@@ -80,10 +90,11 @@ int main(void) {
 	} 
 
 	printf("\n=== 주문 리스트 ===\n");
-	printf("품목     수량 사이즈\n");
+	printf("품목  수량  사이즈\n");
 	for (int i = 0; i < idx; i++) {
-		printf("%s %5d %5s", pro[i].name, pro[i].qty, pro[i].size);
+		printf("%s %d %s", prd[i].name, prd[i].qty, prd[i].size);
 	}
 
 	return 0; 
 }
+// https://dailylifestory.tistory.com/147
