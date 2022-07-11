cfe669b38f5a72d4df6dcd0047931b3cffa83f88
DaanChoi <104367835+DaanChoi@users.noreply.github.com>
2022-07-11 19:49:18 +0900

    .

--- kiosk1.c
+++ kiosk1.c
@@ -7,6 +7,7 @@ typedef struct {
 	char name[20]; // 품목명은 20자 이하
 	int qty;
 	char size[2];
+	int price;
 }
 product;
 
@@ -20,10 +21,11 @@ void receipt(int idx, product prd[], int sum);
 int main(void) {
 	int menu, idx = 0; //메뉴
 	int sum = 0; // 총 금액
-	product prd[] = { 0 }; // 품목
-	char input; //추가 주문 응답
-	char check; //결제 여부 응답
+	product prd[10] = { 0 }; // 품목, 최대 10종류까지 가능
+	// 배열 초기화 때 [크기] 쓰지않으면 자동으로 크기 설정됨. 이때 주의해야 함. 자동으로 설정된 배열의 크기가 아닌 더 큰 크기의 배열을 사용하고 싶다면 사용자가 크기 설정을 해줘야 함(Run-Time check Failure #2 -stack around the variable'변수' was corrupted)
 	char discnt; //멤버십 할인 선택 응답
+	char check; //결제 여부 응답
+	char input; //추가 주문 응답
 
 	while (1) {
 		printf("\n*******JUICY KIOSK*******\n");
@@ -38,11 +40,12 @@ int main(void) {
 				printf("\nM : 3,500원\n-------------------------\n");
 				printf("품목명을 입력하세요 : ");
 				scanf("%s", &prd[idx].name);
-				printf("수량을 입력하세요 : ");
-				scanf("%d", &prd[idx].qty);
 				printf("사이즈를 입력하세요(M) : ");
 				scanf("%s", &prd[idx].size);
-				sum += seoson(prd[idx].qty, prd[idx].size);
+				printf("수량을 입력하세요 : ");
+				scanf("%d", &prd[idx].qty);
+				prd[idx].price = seoson(prd[idx].qty, prd[idx].size);
+				sum += prd[idx].price;
 				idx++;
 			}
 			else if (menu == 2) {
@@ -51,11 +54,12 @@ int main(void) {
 				printf("\nM : 1,500원 / XL : 2,800원\n-------------------------\n");
 				printf("품목명을 입력하세요 : ");
 				scanf("%s", &prd[idx].name);
-				printf("수량을 입력하세요 : ");
-				scanf("%d", &prd[idx].qty);
 				printf("사이즈를 입력하세요(M / XL) : ");
 				scanf("%s", &prd[idx].size);
-				sum += juice(prd[idx].qty, prd[idx].size);
+				printf("수량을 입력하세요 : ");
+				scanf("%d", &prd[idx].qty);
+				prd[idx].price = juice(prd[idx].qty, prd[idx].size);
+				sum += prd[idx].price;
 				idx++;
 			}
 			else if (menu == 3) {
@@ -64,11 +68,12 @@ int main(void) {
 				printf("\nM : 2,000원 / XL : 3,500원\n------------------------------\n");
 				printf("품목명을 입력하세요 : ");
 				scanf("%s", &prd[idx].name);
-				printf("수량을 입력하세요 : ");
-				scanf("%d", &prd[idx].qty);
 				printf("사이즈를 입력하세요(M / XL) : ");
 				scanf("%s", &prd[idx].size);
-				sum += coffee(prd[idx].qty, prd[idx].size);
+				printf("수량을 입력하세요 : ");
+				scanf("%d", &prd[idx].qty);
+				prd[idx].price = coffee(prd[idx].qty, prd[idx].size);
+				sum += prd[idx].price;
 				idx++;
 			}
 			else if (menu == 4) {
@@ -77,11 +82,12 @@ int main(void) {
 				printf("\nM : 2,000원 / XL : 3,800원\n-------------------------\n");
 				printf("품목명을 입력하세요 : ");
 				scanf("%s", &prd[idx].name);
-				printf("수량을 입력하세요 : ");
-				scanf("%d", &prd[idx].qty);
 				printf("사이즈를 입력하세요(M / XL) : ");
 				scanf("%s", &prd[idx].size);
-				sum += yogult(prd[idx].qty, prd[idx].size);
+				printf("수량을 입력하세요 : ");
+				scanf("%d", &prd[idx].qty);
+				prd[idx].price = yogult(prd[idx].qty, prd[idx].size);
+				sum += prd[idx].price;
 				idx++;
 			}
 			else if (menu == 5) {
@@ -90,11 +96,12 @@ int main(void) {
 				printf("\nM : 3,800원\n-------------------------\n");
 				printf("품목명을 입력하세요 : ");
 				scanf("%s", &prd[idx].name);
-				printf("수량을 입력하세요 : ");
-				scanf("%d", &prd[idx].qty);
 				printf("사이즈를 입력하세요(M) : ");
 				scanf("%s", &prd[idx].size);
-				sum += ade(prd[idx].qty, prd[idx].size);
+				printf("수량을 입력하세요 : ");
+				scanf("%d", &prd[idx].qty);
+				prd[idx].price = ade(prd[idx].qty, prd[idx].size);
+				sum += prd[idx].price;
 				idx++;
 			}
 			else {
@@ -107,14 +114,14 @@ int main(void) {
 			}
 		}
 
-		printf("주문 리스트를 확인해 주세요.");
-		printf("\n\n===== 주문 리스트 =====\n");
-		printf("품목  수량  사이즈\n");
+		printf("\n주문 리스트를 확인해 주세요.");
+		printf("\n\n======= 주문 리스트 =======\n");
+		printf("품목          수량  사이즈\n");
 		for (int i = 0; i < idx; i++) {
-			printf("%s  %d  %s\n", prd[i].name, prd[i].qty, prd[i].size);
+			printf("%-10s  %4d  %4s\n", prd[i].name, prd[i].qty, prd[i].size);
 		}
 		printf("\n\n총 금액 : %d원\n", sum); 
-		printf("=======================\n");
+		printf("=========================\n");
 
 		printf("결제하시겠습니까?(y/n) : ");
 		scanf(" %c", &check);
@@ -125,9 +132,9 @@ int main(void) {
 		sum = 0;
 		getchar();
 	}
-	
+	getchar();
 	printf("\n멤버십 할인을 하시겠습니까?(y/n) {VIP등급 10%% 할인 (월 1회만 가능)} : ");
-	scanf(" %c", &discnt);
+	scanf("%c", &discnt);
 	if (discnt == 'y') {
 		sum *= 0.9;
 		printf("10%% 할인됐습니다. 할인된 가격 %d원으로 결제됩니다.\n", sum);
@@ -183,16 +190,16 @@ void receipt(int idx, product prd[], int sum) {
 	printf("=====================================\n");
 	printf("               영수증                \n");
 	printf("=====================================\n");
-	printf("[사업장] 주씨 단국대점\n");
+	printf("[사업장] 쥬씨 단국대점\n");
 	printf("[사업자] 106-82-02786\n");
 	printf("[주소] 경기 용인시 수지구 죽전로 152-1\n");
 	printf("[전화번호] 031-262-2770\n");
 	printf("[매출일] %d-%d-%d  %d:%d:%d\n", tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 	printf("[주문번호] 0050\n");
 	printf("-------------------------------------\n");
-	printf("상품명           수량           금액\n");
+	printf("상품명             수량    금액\n");
 	for (int i = 0; i < idx; i++) {
-		printf("%s %10d %10s\n", prd[i].name, prd[i].qty, prd[i].size);
+		printf("%s(%s) %8d %8d\n", prd[i].name, prd[i].size, prd[i].qty, prd[i].price);
 	}
 	printf("-------------------------------------\n");
 	printf("합계금액                    %d원\n", sum);
@@ -201,25 +208,25 @@ void receipt(int idx, product prd[], int sum) {
 
 
 	//파일에 출력(쓰기)
+
 	fputs("=====================================\n", fp);
 	fputs("               영수증                \n", fp);
 	fputs("=====================================\n", fp);
-	fputs("[사업장] 주씨 단국대점\n", fp);
+	fputs("[사업장] 쥬씨 단국대점\n", fp);
 	fputs("[사업자] 106-82-02786\n", fp);
 	fputs("[주소] 경기 용인시 수지구 죽전로 152-1\n", fp);
 	fputs("[전화번호] 031-262-2770\n", fp);
 	fprintf(fp, "%s %d%c%d%c%d  %d%c%d%c%d\n", "[매출일]", tm.tm_year + 1900, '-', tm.tm_mon + 1, '-', tm.tm_mday, tm.tm_hour, ':', tm.tm_min, ':', tm.tm_sec);
 	fputs("[주문번호] 0050\n", fp);
 	fputs("-------------------------------------\n", fp);
-	fputs("상품명           수량           금액\n", fp);
+	fputs("상품명             수량    금액\n", fp);
 	for (int i = 0; i < idx; i++) {
-		fprintf(fp, "%s %10d %10s\n", prd[i].name, prd[i].qty, prd[i].size);
+		fprintf(fp, "%s(%s) %8d %8d\n", prd[i].name, prd[i].size, prd[i].qty, prd[i].price);
 	}
 	fputs("-------------------------------------\n", fp);
 	fprintf(fp, "%s                    %d%s\n", "합계금액", sum, "원");
 	fputs("=====================================\n", fp);
 	fputs("    ***신용승인정보(고객용)[1]***   \n", fp);
 	
-
 	fclose(fp);
 }
